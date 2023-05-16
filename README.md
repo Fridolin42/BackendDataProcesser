be24ebdcf6bc0b098b8738d5300a2039d25979a9
Fridolin <fridolin_1@yahoo.com>
2023-05-16 20:47:45 +0200

    Started with the Entrypoint Function handler, parser for * ready

--- README.md
+++ README.md
@@ -3,19 +3,55 @@
 <p>Welcome to the data processor for the backend</p>
 
 ## Databases
-### Create new Database
-1. Go in the config folder
-2. Open `databases.json`
-3. Add in the list a new Entry
-   - Structure for objects:
+### Create new Entrypoint
+   - Example Entrypoint
 ```json
-[
-  {
-    "database": "exampleDB.json",
-    "structure": {
-      "typ": "object",
-      "keywords": []
+{
+  "endpoints": [
+    {
+      "url": "/api/users/login",
+      "httpMethod": "post",
+      "requiredPermission": "none",
+      "function": "login"
+    },
+    {
+      "url": "/api/users/signup",
+      "httpMethod": "post",
+      "requiredPermission": "none",
+      "function": "signup"
+    },
+    {
+      "url": "/api/users/info",
+      "httpMethod": "get",
+      "requiredPermission": "login",
+      "function": "getUserInfo()"
+    }
+  ],
+  "responses": {
+    "getUserInfo": {
+      "users[email:*email]": {
+        "username": "username",
+        "email": "email",
+        "exercises": "exercise"
+      }
     }
   }
-]
+}
+
+
 ```
+   - Path: The path of the entrypoint
+   - method: get/post/put/delete
+   - Permissions
+     - 0: No Permission
+     - login: user must me logged in
+   - action: login/signup/response/insert
+   - response (optional): Contains the database names and paths to the data, which will be returned
+     - The Object inside contains the keyword to the specific data as the keywords and the keywords from the response for the values as the value (Exception: `+`)
+     - If you want to search in a list for an object with a special value, use: `[key:*value]`
+       - The `*` indicate, that this is a value from the cookies
+       - The `#` indicate, that this is a value from the body
+       - The `°` indicate, that this is a value from is an url parameter (like .../?q=hello+world)
+       - The `~` indicate, that this is a value from the parameter from the function call
+     - The `+` indicate, that this value will be overridden with the value from the value
+       
--- src/entrypoint.js
+++ src/entrypoint.js
@@ -1,47 +1,84 @@
 const express = require("express")
 const router = express.Router()
 const users = require("./users")
+const dbHandler = require("./dbHandler")
 module.exports = class Entrypoint {
-    constructor(path, method, permission, exercise) {
-        this.path = path
-        this.method = method
-        this.permission = permission
-        this.exercise = exercise
-        switch (method) {
+    static {
+        //TODO read functions
+    }
+    static functions = []
+
+    constructor(url, httpMethod, requiredPermission, exercise) {
+        this.url = url
+        this.method = httpMethod
+        this.requiredPermission = requiredPermission
+        this.functionName = exercise
+        switch (httpMethod) {
             case "get":
-                router.get(path, this.entrypoint);
+                router.get(url, this.entrypoint);
                 break;
             case "post":
-                router.post(path, this.entrypoint);
+                router.post(url, this.entrypoint);
                 break;
             case "put":
-                router.put(path, this.entrypoint);
+                router.put(url, this.entrypoint);
                 break;
             case "delete":
-                router.delete(path, this.entrypoint);
+                router.delete(url, this.entrypoint);
                 break;
             case "all":
-                router.all(path, this.entrypoint);
+                router.all(url, this.entrypoint);
                 break;
             default:
-                console.error("Method " + method + " not supported")
+                console.error("Method " + httpMethod + " not supported")
                 process.exit(10)
 
         }
     }
 
     entrypoint(req, res) {
-        const authorised = this.checkPermission(req)
-        if (!authorised) return res.send({"status": "failed"})
+        if (this.requiredPermission === "login" && !this.checkPermission(req)) return res.send({"status": "failed"})
+        Entrypoint.functions[this.functionName].execute(req, res)
     }
 
     checkPermission(req) {
-        const permission = this.permission
+        const permission = this.requiredPermission
         if (permission === "0") return true
         if (permission === "login") {
             const email = req.cookies.email
             const password = req.cookies.password
             return users.login(email, password)
+        }
+    }
+}
+
+class EntrypointFunction {
+
+    constructor(name) {
+        this.function = require("../config/entrypoints.json")
+    }
+
+    execute(req, res) {
+        for (let dbPath of Object.keys(this.function)) {
+            let dbPathParts = Array.from(dbPath)
+            //parser for the path
+            for (let i = 0; i < dbPathParts.length; i++) {
+                const dbPathPart = dbPathParts[i]
+                switch (dbPathPart) {
+                    case '*': {
+                        let end = null
+                        for (let j = i + 1; j < dbPathParts.length; j++) {
+                            if ('.,]'.includes(dbPathParts[j])) end = j
+                        }
+                        if (end == null) throw new Error("Wrong Syntax in entrypoints.json")
+                        //i + 1 is correct, because I don't wont to have the '*' in my string
+                        let key = dbPath.substring(i + 1, end)
+                        let value = req.cookies[key]
+                        dbPath = dbPath.substring(0, i) + value + dbPath.substring(end)
+                    }
+                }
+            }
+
         }
     }
 }
